3fa6028b3cf23331a2bc3dde0348ca84945bd659
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-04-24 16:31:16 +0100

    [ travis ] force reinstall

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -3,18 +3,16 @@
 VERSION=2.6.1
 CURRENT=$(agda -V | sed "s/Agda version \([^-]*\).*/\1/")
 
-if ! type "agda" > /dev/null || [ ! "$CURRENT" = "$VERSION" ]; then
-  cabal update
-  cabal install alex happy cpphs
-  git clone https://github.com/agda/agda --depth=1
-  cd agda
-  cabal install
-  cd ..
-  mkdir -p $HOME/.agda
-  cp libraries $HOME/.agda/
-  cd $HOME/.agda/
-  git clone https://github.com/agda/agda-stdlib
-  cd agda-stdlib
-  git checkout experimental
-  cd -
-fi
+cabal update
+cabal install alex happy cpphs
+git clone https://github.com/agda/agda --depth=1
+cd agda
+cabal install
+cd ..
+mkdir -p $HOME/.agda
+cp libraries $HOME/.agda/
+cd $HOME/.agda/
+git clone https://github.com/agda/agda-stdlib
+cd agda-stdlib
+git checkout experimental
+cd -
