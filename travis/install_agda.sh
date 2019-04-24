b21d1b1090914519aa7fc13c16b6e414344ce079
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-04-24 16:54:38 +0100

    [ fix ] missing dependency

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -4,7 +4,7 @@ VERSION=2.6.1
 CURRENT=$(agda -V | sed "s/Agda version \([^-]*\).*/\1/")
 
 cabal update
-cabal install alex happy cpphs
+cabal install alex happy cpphs clock
 git clone https://github.com/agda/agda --depth=1
 cd agda
 cabal install
